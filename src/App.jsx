49cf041f89a94b9f914330bdbdf0db17845886b3
seymzr <seymzr@gmail.com>
2024-05-22 09:14:45 +0400

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import BrandsCarousel from './components/General/BrandsCarousel'
 import Advantages from './components/Home/Advantages'
+import ProductsGrid from './components/Home/ProductsGrid'
 import Footer from './components/UI/Footer'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     {/* <BrandsCarousel /> */}
+    <ProductsGrid />
     <Advantages />
       <Footer />
     </>
