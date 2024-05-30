4cd9fb4a05adc01b0c21b8d0262140254853b4f4
seymzr <seymzr@gmail.com>
2024-05-30 09:53:38 +0400

    s

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ProductsGrid from "../components/Home/ProductsGrid";
 import Advantages from "../components/Home/Advantages";
 import SaleProducts from "../components/Home/SaleProducts";
+import Categories from "./Categories";
 
 const Home = () => {
   return (
     <>
+      <Categories />
       <ProductsGrid />
       <Advantages />
       <SaleProducts />
