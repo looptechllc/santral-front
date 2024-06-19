e1de8e5e5f33a1cb6cfd55873c19ba76a657e873
seymzr <seymzr@gmail.com>
2024-06-19 04:01:24 +0400

    upper banner done

--- src/components/Home/ProductsGrid.jsx
+++ src/components/Home/ProductsGrid.jsx
@@ -1,22 +1,33 @@
 import React from "react";
-import skyline from "../../assets/skyline.png";
-import stihl from "../../assets/stihl.png";
-import gardena from "../../assets/gardena.png";
+import banner1 from "../../assets/upperbanner1.png";
+import banner2 from "../../assets/upperbanner2.png";
+import banner3 from "../../assets/upperbanner3.png";
 const ProductsGrid = () => {
   return (
-    <div className="md:w-[95%] mx-auto px-3 my-[60px] grid grid-cols-2 gap-[24px]">
-      <div className="  w-full row-span-2 rounded-[16px] overflow-hidden  relative">
-        <img src={skyline} alt="skyline" className="object-cover" />
-        <div className="absolute w-full bottom-0 bg-[#fff]/70 py-[16px] text-[20px] text-center font-medium"> Skyline design mebellər </div>
+    <div className="md:w-[95%] mx-auto px-3 my-[60px] flex  gap-[24px]">
+      <div className="rounded-[16px] overflow-hidden   relative">
+        <img src={banner1} alt="gardena" className="object-cover" />
+       
+      </div>
+      <div className="flex flex-col gap-[24px] ">
+      
+      <div className="  w-full  rounded-[16px] overflow-hidden  relative">
+        <img src={banner2} alt="skyline" className="object-cover" />
+        {/* <div className="absolute  top-[24px] left-[24px] bg-[#000]/50 rounded-[8px] px-[16px] py-[12px] ">
+          {" "}
+          <h4 className="text-[20px] text-left font-medium text-white">Scheinder məhsulları</h4>
+          <p className="text-white font-[500]">Evinizi orijinal üslubla təchiz etmək üçün ən yaxşı mebellər və ən son dizayn</p>
+        </div> */}
       </div>
       <div className="rounded-[16px] overflow-hidden w-full relative">
-        <img src={stihl} alt="stihl" className="object-cover" />
-        <div className="absolute  bottom-0 right-0 bg-[#fff]/70 p-[16px] rounded-tl-[16px] text-[20px] text-center font-medium"> Stihl məhsulları</div>
+        <img src={banner3} alt="stihl" className="object-cover" />
+        {/* <div className="absolute  bottom-0 right-0 bg-[#fff]/70 p-[16px] rounded-tl-[16px] text-[20px] text-center font-medium">
+          {" "}
+          Stihl məhsulları
+        </div> */}
       </div>
-      <div className="rounded-[16px] overflow-hidden  w-full relative">
-        <img src={gardena} alt="gardena" className="object-cover" />
-        <div className="absolute  bottom-0 left-0 bg-[#fff]/70 p-[16px] rounded-tr-[16px] text-[20px] text-center font-medium"> Gardena məhsulları</div>
       </div>
+      
     </div>
   );
 };
