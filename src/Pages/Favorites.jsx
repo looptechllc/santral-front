5166065f9aa64fac3ee20c195bcc355e73d8bc59
seymzr <seymzr@gmail.com>
2024-07-21 18:56:20 +0400

    favorite page

--- src/Pages/Favorites.jsx
+++ src/Pages/Favorites.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import secureLocalStorage from 'react-secure-storage';
+import ElementCard from '../components/General/ElementCard';
 
 const Favorites = () => {
+    const [favorites,setFavorites] = useState();
+    async function fetchFavorites() {
+        const accessToken = secureLocalStorage.getItem("access_token");
+        const url = `https://api.santral.az/v1/customers/favorites/`;
+    
+        try {
+          const response = await fetch(url, {
+            method: "GET",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${accessToken}`,
+            },
+            // body: JSON.stringify({}),
+          });
+          const data = await response.json();
+          if (response.ok) {
+            console.log(data);
+            setFavorites(data);
+            
+          } else {
+            console.log("error");
+          }
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      }
+
+
+      useEffect(()=>{
+        fetchFavorites()
+      },[])
   return (
-    <div>Favorites</div>
+    <div className='px-[48px] pt-[24px] pb-[108px]'>
+        <div>
+        <h2 className='text-[32px] text-black/90'>Seçilmişlər</h2>
+        </div>
+        <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-[24px] my-[24px]'>
+            {favorites?.map((item,index)=>(
+                <ElementCard id ={item.id} img={`https://cdn.santral.az//images/${item.thumbnail}`} sale={0}  name={item.title} price={item.price}  beforePrice={item.oldPrice} isLiked={true}  link={item.title}/>
+            ))}
+        </div>
+    </div>
   )
 }
 
