9304b34cda409f49b7c4771d5634c00cc9979b54
seymzr <seymzr@gmail.com>
2024-07-31 08:13:31 +0400

    item description and similar items slider

--- src/components/ProductPage/ProductInfo.jsx
+++ src/components/ProductPage/ProductInfo.jsx
@@ -8,6 +8,7 @@ import share from "../../assets/share.svg";
 import heartFill from "../../assets/heartFill.svg";
 import ReactImageGallery from "react-image-gallery";
 import ElementCard from "../General/ElementCard";
+import Slider from "react-slick";
 const ProductInfo = () => {
   const { name } = useParams();
   const [description, setDescription] = useState("");
@@ -23,7 +24,7 @@ const ProductInfo = () => {
           `https://api.santral.az/v1/routes/find?domain=santral_www&location=/az/products/${name}`
         );
         const data = await response.json();
-        // console.log(data);
+        console.log(data);
         setDescription(data.route);
         setCount(data.route.minimalOrder);
       } catch (error) {
@@ -75,7 +76,42 @@ const ProductInfo = () => {
     description?.images?.length > 0
       ? description?.images?.map((item) => item)
       : [description.thumbnail];
-  console.log(images);
+
+  const productSettings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+autoplaySpeed:2500,
+    cssEase: "linear",
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: false,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <div className="md:w-[95%] mx-auto pb-[120px] ">
       <div className="flex flex-col md:flex-row gap-[24px] my-[24px]">
@@ -137,55 +173,54 @@ const ProductInfo = () => {
                 </p>
               )}
               <p className="text-[#777] font-light">
-                SKU: {description?.title}
+                SKU: {description?.brandCode}
               </p>
             </div>
           </div>
 
           <div className=" flex w-full items-center justify-between border-b border-[#eaeaea] py-[24px]">
             <div className="flex items-center gap-[10px] p-[6px]  border border-[#EAEAEA] w-fit rounded-[32px]">
-            <button
-  className="duration-300 bg-white hover:bg-[#FFD23F] text-black/40 hover:text-white w-[40px] h-[40px] rounded-full text-[32px] flex items-center justify-center p-0 group"
-  onClick={decrement}
->
-  <svg
-    className="fill-current text-black/40 group-hover:text-white"
-    width="18"
-    height="2"
-    viewBox="0 0 18 2"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M17 2H1C0.453333 2 0 1.54667 0 1C0 0.453333 0.453333 0 1 0H17C17.5467 0 18 0.453333 18 1C18 1.54667 17.5467 2 17 2Z"
-      fill="currentColor"
-    />
-  </svg>
-</button>
-<span className="text-[20px] font-medium">{count}</span>
-<button
-  className="duration-300 bg-white hover:bg-[#FFD23F] text-black/40 hover:text-white w-[40px] h-[40px] rounded-full text-[32px] flex items-center justify-center p-0 group"
-  onClick={increment}
->
-  <svg
-    className="fill-current text-black/40 group-hover:text-white"
-    width="32"
-    height="32"
-    viewBox="0 0 32 32"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M24 17H8C7.45333 17 7 16.5467 7 16C7 15.4533 7.45333 15 8 15H24C24.5467 15 25 15.4533 25 16C25 16.5467 24.5467 17 24 17Z"
-      fill="currentColor"
-    />
-    <path
-      d="M16 25C15.4533 25 15 24.5467 15 24V8C15 7.45333 15.4533 7 16 7C16.5467 7 17 7.45333 17 8V24C17 24.5467 16.5467 25 16 25Z"
-      fill="currentColor"
-    />
-  </svg>
-</button>
-
+              <button
+                className="duration-300 bg-white hover:bg-[#FFD23F] text-black/40 hover:text-white w-[40px] h-[40px] rounded-full text-[32px] flex items-center justify-center p-0 group"
+                onClick={decrement}
+              >
+                <svg
+                  className="fill-current text-black/40 group-hover:text-white"
+                  width="18"
+                  height="2"
+                  viewBox="0 0 18 2"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M17 2H1C0.453333 2 0 1.54667 0 1C0 0.453333 0.453333 0 1 0H17C17.5467 0 18 0.453333 18 1C18 1.54667 17.5467 2 17 2Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </button>
+              <span className="text-[20px] font-medium">{count}</span>
+              <button
+                className="duration-300 bg-white hover:bg-[#FFD23F] text-black/40 hover:text-white w-[40px] h-[40px] rounded-full text-[32px] flex items-center justify-center p-0 group"
+                onClick={increment}
+              >
+                <svg
+                  className="fill-current text-black/40 group-hover:text-white"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M24 17H8C7.45333 17 7 16.5467 7 16C7 15.4533 7.45333 15 8 15H24C24.5467 15 25 15.4533 25 16C25 16.5467 24.5467 17 24 17Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M16 25C15.4533 25 15 24.5467 15 24V8C15 7.45333 15.4533 7 16 7C16.5467 7 17 7.45333 17 8V24C17 24.5467 16.5467 25 16 25Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </button>
             </div>
             <p className="text-[#FD8521] text-[32px] font-bold">
               {(count * description?.price).toFixed(2)} ₼
@@ -301,7 +336,7 @@ const ProductInfo = () => {
         </div>
       </div>
       <div className="flex items-center gap-[16px]">
-        <button
+        {description?.parameters?.length > 0 && description?.desc && <button
           onClick={() => {
             setSelectedDetail(0);
           }}
@@ -310,7 +345,7 @@ const ProductInfo = () => {
           }`}
         >
           Xüsusiyyətlər
-        </button>
+        </button>}
         <button
           onClick={() => {
             setSelectedDetail(1);
@@ -339,9 +374,11 @@ const ProductInfo = () => {
           )}
         </div>
       ) : (
-        <div className="bg-white drop-shadow-sm border border-solid border-[#EAEAEA] rounded-[16px] p-[18px] grid  gap-[24px] grid-cols-1 md:grid-cols-4 place-items-center">
+        <div className="bg-white drop-shadow-sm border border-solid border-[#EAEAEA] rounded-[16px] p-[18px] ">
+          <Slider {...productSettings}>
           {relatedProducts?.map((item, index) => (
-            <ElementCard
+            <div className="px-[10px]" key={index}>
+              <ElementCard
               id={item?.id}
               img={`https://cdn.santral.az//images/${item.thumbnail}`}
               sale={item.discountPercent}
@@ -350,7 +387,9 @@ const ProductInfo = () => {
               beforePrice={item.oldPrice}
               link={item.name}
             />
+            </div>
           ))}
+          </Slider>
         </div>
       )}
     </div>
