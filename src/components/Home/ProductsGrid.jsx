f350ad3411a1c4a76c8555a06757b3013a636699
seymzr <seymzr@gmail.com>
2024-08-06 08:29:50 +0400

    upper banner

--- src/components/Home/ProductsGrid.jsx
+++ src/components/Home/ProductsGrid.jsx
@@ -7,27 +7,45 @@ const ProductsGrid = () => {
     <div className="md:w-[95%] mx-auto px-3 my-[60px] flex flex-col md:flex-row  gap-[24px]">
       <div className="rounded-[16px] overflow-hidden   relative">
         <img src={banner1} alt="gardena" className="object-cover" />
-       
+        <div className="absolute  top-[24px] left-[24px] bg-[#000]/50 text-white duration-300 hover:bg-[#FFD23F]/80 hover:text-black rounded-[8px] px-[16px] py-[12px] ">
+          {" "}
+          <h4 className="text-[20px] text-left font-medium">
+            Bağ Mebelləri
+          </h4>
+          <p className="font-[500]">
+            Evinizi orijinal üslubla təchiz etmək üçün ən yaxşı mebellər və ən
+            son dizayn
+          </p>
+        </div>
       </div>
       <div className="flex flex-col gap-[24px] ">
-      
-      <div className="  w-full  rounded-[16px] overflow-hidden  relative">
-        <img src={banner2} alt="skyline" className="object-cover" />
-        {/* <div className="absolute  top-[24px] left-[24px] bg-[#000]/50 rounded-[8px] px-[16px] py-[12px] ">
-          {" "}
-          <h4 className="text-[20px] text-left font-medium text-white">Scheinder məhsulları</h4>
-          <p className="text-white font-[500]">Evinizi orijinal üslubla təchiz etmək üçün ən yaxşı mebellər və ən son dizayn</p>
-        </div> */}
+        <div className="  w-full  rounded-[16px] overflow-hidden  relative">
+          <img src={banner2} alt="skyline" className="object-cover" />
+          <div className="absolute  top-[24px] left-[24px] bg-[#000]/50 text-white duration-300 hover:bg-[#FFD23F]/80 hover:text-black rounded-[8px] px-[16px] py-[12px] ">
+            {" "}
+            <h4 className="text-[20px] text-left font-medium ">
+              Bağçılıq
+            </h4>
+            <p className=" font-[500]">
+              Evinizi orijinal üslubla təchiz etmək üçün ən yaxşı mebellər və ən
+              son dizayn
+            </p>
+          </div>
+        </div>
+        <div className="rounded-[16px] overflow-hidden w-full relative">
+          <img src={banner3} alt="stihl" className="object-cover" />
+          <div className="absolute  top-[24px] left-[24px] bg-[#000]/50 text-white duration-300 hover:bg-[#FFD23F]/80 hover:text-black rounded-[8px] px-[16px] py-[12px] ">
+            {" "}
+            <h4 className="text-[20px] text-left font-medium ">
+              Ev Mebelləri
+            </h4>
+            <p className=" font-[500]">
+              Evinizi orijinal üslubla təchiz etmək üçün ən yaxşı mebellər və ən
+              son dizayn
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="rounded-[16px] overflow-hidden w-full relative">
-        <img src={banner3} alt="stihl" className="object-cover" />
-        {/* <div className="absolute  bottom-0 right-0 bg-[#fff]/70 p-[16px] rounded-tl-[16px] text-[20px] text-center font-medium">
-          {" "}
-          Stihl məhsulları
-        </div> */}
-      </div>
-      </div>
-      
     </div>
   );
 };
